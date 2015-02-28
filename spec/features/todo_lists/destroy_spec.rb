7c60d8b7a951f459497f53565b7c7baab1ed99be
Andrey <frolin@undev.ru>
2015-02-28 17:23:23 +0300

    Test destroy todo lists

---
+++ spec/features/todo_lists/destroy_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+
+describe 'Deleting todo lists' do
+
+  let!(:todo_list) {TodoList.create(title: "My edit todo list", description: "edit me")}
+
+  it 'is successful destroy' do
+
+    visit "/todo_lists"
+
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Destroy"
+    end
+
+    expect(page).to_not have_content(todo_list.title)
+    expect(TodoList.count).to eq(0)
+
+  end
+
+end
