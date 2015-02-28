25c8a63912c724bf44ead47004c7191962384be7
Andrey <frolin@undev.ru>
2015-02-28 22:11:17 +0300

    Add create task spec

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,36 @@
 class TasksController < ApplicationController
+
+  before_action :set_todo_list_id, only: [:index, :new]
+
   def index
+  end
+
+  def new
+    @task = @todo_list.tasks.new
+  end
+
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @task      = @todo_list.tasks.new(task_params)
+    if @task.save
+      flash[:success] = "Added new task"
+      redirect_to todo_list_tasks_path
+    else
+      flash[:error] = "There was a problem to add new task"
+      render :new
+    end
+
+  end
+
+  private
+
+  def set_todo_list_id
     @todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def task_params
+    params[:task].permit(:content)
+  end
+
 end
---
+++ spec/features/tasks/create_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+
+describe 'Adding Todo_list tasks ' do
+  let!(:todo_list) {TodoList.create(title: "My tewst todo_list", description: "Test description")}
+
+  def visit_todo_list(list)
+    visit "/todo_lists"
+
+    within "#todo_list_#{todo_list.id}" do
+      click_link "List items"
+    end
+  end
+
+
+  it 'is successful with valid content' do
+    visit_todo_list(todo_list)
+    click_link "Add"
+    fill_in 'Content', with: "Fruits"
+    click_button 'Add'
+    expect(page).to have_content("Added new task")
+    within('div.list-group') do
+      expect(page).to have_content("Fruits")
+    end
+  end
+end
